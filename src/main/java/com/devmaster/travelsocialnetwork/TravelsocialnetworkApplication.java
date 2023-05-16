87a1f90f5a58e12d612089e97ffe3634ccaa2d06
HoaCoder <VoThaiHoa19102003@gmail.com>
2023-05-16 22:04:31 +0700

    version 1

---
+++ src/main/java/com/devmaster/travelsocialnetwork/TravelsocialnetworkApplication.java
@@ -0,0 +1,13 @@
+package com.devmaster.travelsocialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelsocialnetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelsocialnetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devmaster/travelsocialnetwork/TravelsocialnetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmaster.travelsocialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelsocialnetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
